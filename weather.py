3e5b320216f312cb26b9361ad3229569832cee9f
Clarence-14 <thisisclarence1@gmail.com>
2025-09-19 19:23:45 +0300

    commit

---
+++ weather.py
@@ -0,0 +1,27 @@
+import requests
+
+api_key = '7352f2447cc7e77b04082d19fc97b843'
+base_url = 'http://api.openweathermap.org/data/2.5/weather?'
+
+user_input = input('Enter city name: ')
+
+weather_data = requests.get(
+    f"https://api.openweathermap.org/data/2.5/weather?q={user_input}&appid={api_key}&units=metric"
+)
+
+if weather_data.status_code != 200:
+    print("City not found. Please check the city name and try again.")
+    
+else:
+    weather = weather_data.json()['weather'][0]['main']
+    temperature = weather_data.json()['main']['temp']
+    humidity = weather_data.json()['main']['humidity']  
+    wind_speed = weather_data.json()['wind']['speed']
+    city = weather_data.json()['name']
+    country = weather_data.json()['sys']['country']
+
+print(f"Current weather in {city}, {country}:")
+print(f"Weather: {weather}")
+print(f"Temperature: {temperature}°C")
+print(f"Humidity: {humidity}%")
+print(f"Wind Speed: {wind_speed} m/s")
